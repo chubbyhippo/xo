aa4de04763cc755ec2c9b7532c4cd73ae3c5e744
ch <mkmsacc@outlook.com>
2024-06-20 21:23:01 +0700

    Remove unnecessary print statements
    The code changes remove three redundant System.out.println() calls from the XO.java file. These print statements were originally used for debugging purposes and are no longer necessary. The removal of these lines makes the code cleaner and easier to read.

--- XO.java
+++ XO.java
@@ -6,9 +6,7 @@ public class XO {
     public static void main(String... args) {
         var zipFile = new File(args[0]);
         var zipFileAbsolutePath = zipFile.getAbsolutePath();
-        System.out.println(zipFileAbsolutePath);
         var folder = new File(zipFileAbsolutePath.substring(0, zipFileAbsolutePath.lastIndexOf(".")));
-        System.out.println(folder.getAbsolutePath());
         runCommand("unzip", zipFileAbsolutePath);
         String idea;
         if (System.getProperty("os.name").contains("Windows")) {
@@ -17,10 +15,10 @@ public class XO {
             idea = "idea";
         }
 
+
         Stream.of("build.gradle.kts", "pom.xml", "build.gradle")
                 .map(it -> new File(folder, it))
                 .filter(File::exists)
-                .peek(file -> System.out.println(file.getAbsolutePath()))
                 .map(it -> new String[]{idea, it.getAbsolutePath()})
                 .forEach(XO::runCommand);
     }
